8552b481b6d7e7a073bb79902768ae3395b32118
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-11 19:25:25 -0300

    Backend #1 - Consign e Middlewares.Js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
 const app = require('express')()
+const consign = require('consign')
+
+consign()
+    .then('./config/middlewares.js')
 
 app.listen(3000, () => {
     console.log('Backend escutando!');

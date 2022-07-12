72324c098d23cdd85245a0610391750e130a987c
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-11 23:58:13 -0300

    Backend #7 - Api de Categoria #01

--- backend/api/validation.js
+++ backend/api/validation.js
@@ -1,14 +1,14 @@
 module.exports = app => {
     function existsOrError(value, msg) {
         if (!value) throw msg
-        if (Array.isArray(value) && value === 0) throw msg
+        if (Array.isArray(value) && value.length === 0) throw msg
         if (typeof value === 'string' && !value.trim()) throw msg
     }
 
     function notExistsOrError(value, msg) {
         try {
             existsOrError(value, msg)
-        } catch (msg) {
+        } catch(msg) {
             return
         }
         throw msg

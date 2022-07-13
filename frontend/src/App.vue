261d6c89e406e518eff6ac20aa9ae010f43a120a
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-13 20:50:04 -0300

    Frontend #1 - Estrutura Inicial do Template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,50 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<HeaderVue/>
+		<MenuVue/>
+		<ContentVue/>
+		<FooterVue/>
 	</div>
 </template>
 
 <script>
+import HeaderVue from './components/template/Header.vue'
+import FooterVue from './components/template/Footer.vue'
+import ContentVue from './components/template/Content.vue'
+import MenuVue from './components/template/Menu.vue'
+
 export default {
 	name: "App",
+	components: {
+		HeaderVue,
+		MenuVue,
+		ContentVue,
+		FooterVue
+	}
 }
 </script>
 
 <style>
+	* {
+		font-family: "Lato", sans-serif;
+	}
 
+	body{
+		margin: 0;
+	}
+
+    #app{
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+
+        height: 100vh;
+        display: grid;
+        grid-template-rows: 60px 1fr 40px;
+        grid-template-columns: 300px 1fr;
+        grid-template-areas:
+            "header header"
+            "menu content"
+            "menu footer";
+
+    }
 </style>
---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer class="footer">
+    <span>Copyright <strong>Irenaldo Júnior</strong> 2022</span>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer{
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+        
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+
+    .app{
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+
+        height: 100vh;
+        display: grid;
+        grid-template-rows: 60px 1fr 40px;
+        grid-template-columns: 300px 1fr;
+        grid-template-areas:
+            "header header"
+            "menu content"
+            "menu footer";
+
+    }
+</style>

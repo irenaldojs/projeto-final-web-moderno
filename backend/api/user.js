ba5156258d58349bfdcf621efdb65328624e875a
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-13 20:08:58 -0300

    Backend 17# - Validar Cadastro de Administradores

--- backend/api/user.js
+++ backend/api/user.js
@@ -12,6 +12,9 @@ module.exports = app => {
         const user = { ...req.body }
         if (req.params.id) user.id = req.params.id
 
+        if (!req.originalUrl.startsWith('/users')) user.admin = false
+        if (!req.user || !req.user.admin) user.admin = false
+
         try {
             existsOrError(user.name, 'Nome não informado')
             existsOrError(user.email, 'E-mail não informado')
@@ -35,6 +38,7 @@ module.exports = app => {
             app.db('users')
                 .update(user)
                 .where({ id: user.id })
+                .whereNull('deletedAt')
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {

7f22ca150dddf67cd502a555c25d900c6dc8e17a
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-14 21:10:45 -0300

    Frontend #6 - Router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import './config/bootstrap'
 import App from './App'
 import store from './config/store'
+import router from './config/router'
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')

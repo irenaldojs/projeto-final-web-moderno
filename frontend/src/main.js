c54b5c9da84a466e976d752eac83af9c0e0f7374
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-14 22:32:27 -0300

    Frontend #7 - Componente Home

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IklyZW5hbGRvIErDum5pb3IgZGEgU2lsdmEiLCJlbWFpbCI6ImlyZW5hbGRvQGNvZGVyLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NTc4NDgwNDIsImV4cCI6MTY1ODEwNzI0Mn0.HS1P9Y6vO0NYh3G6KrTxEhAihUOrVNfXOrjoUWU4O6c'
+
+
 new Vue({
   store,
   router,

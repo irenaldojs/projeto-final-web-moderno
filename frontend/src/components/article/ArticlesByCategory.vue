35aa9c08cdb85546626e1690580b6ce11a607368
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-15 23:38:13 -0300

    Frontend #18 - Artigos por Categorias #03

--- frontend/src/components/article/ArticlesByCategory.vue
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -3,7 +3,7 @@
         <PageTitle icon="fa fa-folder-o" :main="category.name" sub="Categoria" />
         <ul>
             <li v-for="article in articles" :key="article.id">
-                {{ article.name}}
+                <ArticleItem :article="article" />
             </li>
         </ul>
         <div class="load-more">
@@ -18,10 +18,11 @@
 import { baseApiUrl } from '@/global'
 import axios from 'axios'
 import PageTitle from '../template/PageTitle'
+import ArticleItem from './ArticleItem'
 
 export default {
     name: 'ArticlesByCategory',
-    components: { PageTitle },
+    components: { PageTitle, ArticleItem },
     data: function () {
         return {
             category: {},
@@ -41,7 +42,7 @@ export default {
                 this.articles = this.articles.concat(res.data)
                 this.page++
 
-                if(res.data.length === 0) this.loadMore = false
+                if (res.data.length === 0) this.loadMore = false
             })
         }
     },
@@ -54,15 +55,15 @@ export default {
 </script>
 
 <style>
-    .articles-by-category ul{
-        list-style-type: none;
-        padding: 0px;
-    }
+.articles-by-category ul {
+    list-style-type: none;
+    padding: 0px;
+}
 
-    .articles-by-category .load-more{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 25px;
-    }
+.articles-by-category .load-more {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 25px;
+}
 </style>

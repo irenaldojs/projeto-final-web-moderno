f217c16335f946f11324a35a0c3a90e49e63a664
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-18 22:44:19 -0300

    Frontend #23 - Validando Admin

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -6,6 +6,7 @@ import AdminPages from '../components/admin/AdminPages'
 import ArticlesByCategory from '../components/article/ArticlesByCategory'
 import ArticleById from '../components/article/ArticleById'
 import Auth from '../components/auth/Auth'
+import { userKey } from '@/global'
 
 Vue.use(VueRouter)
 
@@ -18,7 +19,8 @@ const routes = [
     {
         name: 'adminPages',
         path: '/admin',
-        component: AdminPages
+        component: AdminPages,
+        meta: { requireAdmin: true }
     },
     {
         name: 'articlesByCategory',
@@ -29,14 +31,28 @@ const routes = [
         name: 'articleById',
         path: '/articles/:id',
         component: ArticleById
-    },{
+    }, {
         name: 'auth',
         path: '/auth',
         component: Auth
     }
 ]
 
-export default new VueRouter({
+const router = new VueRouter({
     mode: 'history',
     routes: routes
 })
+
+router.beforeEach((to, from, next) => {
+    const json = localStorage.getItem(userKey)
+
+    if (to.matched.some(record => record.meta.requireAdmin)) {
+        const user = JSON.parse(json)
+        user && user.admin ? next() : next({ path: '/' })
+    }else{
+        next()
+    }
+
+})
+
+export default router

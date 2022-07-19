5071530592fa3e4c171f98379be6781854c3bd6f
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-18 23:13:38 -0300

    Frontend #24 - Token Expirado

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,15 +6,11 @@ import './config/msgs'
 import App from './App'
 import store from './config/store'
 import router from './config/router'
-import liquorTree from 'liquor-tree'
+import './config/axios'
 
 Vue.config.productionTip = false
 
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IklyZW5hbGRvIErDum5pb3IgZGEgU2lsdmEiLCJlbWFpbCI6ImlyZW5hbGRvQGNvZGVyLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NTc4NDgwNDIsImV4cCI6MTY1ODEwNzI0Mn0.HS1P9Y6vO0NYh3G6KrTxEhAihUOrVNfXOrjoUWU4O6c'
-
-
 new Vue({
-  liquorTree,
   store,
   router,
   render: h => h(App)
